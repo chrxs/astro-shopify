6147cf97106fd5b92641004cb08e90a2f46f3898
Chris Miller (Chrxs) <pirate.chrxs@gmail.com>
2023-09-17 13:21:54 +0100

    Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,20 @@
 import { defineConfig } from "astro/config";
 import netlify from '@astrojs/netlify/functions';
 
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    svelte(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), svelte()]
 });
